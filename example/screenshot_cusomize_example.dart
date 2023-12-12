ad3f25a21269a7558bb198bf1dcd519fee22060b
Minh Dũng Lê <leminhdungltv@gmail.com>
2023-12-12 10:13:27 +0700

    init project

---
+++ example/screenshot_cusomize_example.dart
@@ -0,0 +1,6 @@
+import 'package:screenshot_cusomize/screenshot_cusomize.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/screenshot_cusomize.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library screenshot_cusomize;
+
+export 'src/screenshot_cusomize_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
